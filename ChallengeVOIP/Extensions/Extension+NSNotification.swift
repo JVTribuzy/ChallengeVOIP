2d83e8cc19345ec4500f1b0968b74e820ce067e8
João Victor Moreira Tribuzy <jvtribuzy@gmail.com>
2020-01-07 16:24:08 -0400

    Organizing files to implement JSON Parsing

--- ChallengeVOIP/Extensions/Extension+NSNotification.swift
+++ ChallengeVOIP/Extensions/Extension+NSNotification.swift
@@ -1,8 +1,8 @@
 //
-//  FakeDB.swift
+//  Extension+NSNotification.swift
 //  ChallengeVOIP
 //
-//  Created by João Tribuzy on 06/01/20.
+//  Created by João Tribuzy on 07/01/20.
 //  Copyright © 2020 João Tribuzy. All rights reserved.
 //
 
